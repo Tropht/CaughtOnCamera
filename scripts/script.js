af3ed092e99d90cd672d6d464278deb87922828e
Blake Stevens <tropht@gmail.com>
2015-10-15 12:14:20 -0600

    added styling to the top elements of the graphic. Also added small functionality when you scroll over the images in the top sections

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+    $('.meerkat').mouseover(function(){
+      $('.meerkat').css("background-image","url('img/meerkat.png')");
+    });
+    $('.periscope').mouseover(function(){
+      $('.periscope').css("background-image","url('img/periscope.png')");
+    });
+});
