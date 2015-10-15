43919d8c2822ba249559d71661b8426bfacf1020
Blake Stevens <tropht@gmail.com>
2015-10-15 14:53:45 -0600

    worked on middle section. Added SVG circles and added MagicScroll to make them appear. Also add all the images in the middle section and aligned them properly

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,11 @@ $( document ).ready(function() {
       $('.periscope').css("background-image","url('img/periscope.png')");
     });
 });
+
+// ScrollMagic
+var controller = new ScrollMagic.Controller();
+
+var scene = new ScrollMagic.Scene({triggerElement: ".middle"})
+					// trigger a velocity opaticy animation
+					.setVelocity(".circle", {opacity: 0.7}, {duration: 500})
+					.addTo(controller);
