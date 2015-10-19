df3e85906e7fc1d133dd8052391d61813653b4fb
Blake Stevens <tropht@gmail.com>
2015-10-19 11:31:38 -0600

    I did stuff

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,9 @@ $( document ).ready(function() {
     $('.periscope').mouseover(function(){
       $('.periscope').css("background-image","url('img/periscope.png')");
     });
-
+    $(function() {
+    $( ".bottom" ).accordion();
+  });
 
 });
 
@@ -17,3 +19,18 @@ var scene = new ScrollMagic.Scene({triggerElement: ".middle"})
 					.setVelocity(".circle", {opacity: 0.7}, {duration: 500})
           .setClassToggle(".diagramImg", "moveDown")
 					.addTo(controller);
+
+var header1 = new ScrollMagic.Scene({triggerElement: "#mBubble"})
+          .setVelocity("#header1", {opacity: 1}, {duration: 500})
+          .setClassToggle('#header1', "moveRight")
+          .addTo(controller);
+
+var header2 = new ScrollMagic.Scene({triggerElement: "#header1"})
+          .setVelocity("#header2", {opacity: 1}, {duration: 500})
+          .setClassToggle('#header2', "moveRight")
+          .addTo(controller);
+
+var header3 = new ScrollMagic.Scene({triggerElement: "#header2"})
+          .setVelocity("#header3", {opacity: 1}, {duration: 500})
+          .setClassToggle('#header3', "moveRight")
+          .addTo(controller);
